c159527dcb732269eb696ca9a58f0671f88f08b8
Naftali Weiss <Naftali@PROPUMP.local>
2021-02-15 12:45:38 -0500

    got the excel data processing back on line

--- public/js/DataP.js
+++ public/js/DataP.js
@@ -1,4 +1,24 @@
 export const dp = ['$http', function($http){
     const ctrl = this; 
     
+    this.newFile = {}
+
+    this.showFile = function(){
+        const myFile = document.getElementById("fileBtn");
+        // console.log(myFile.files[0])
+
+        let reader = new FileReader()
+        reader.readAsBinaryString(myFile.files[0])
+        reader.onload = (e) => {
+            let data = e.target.result
+            let wb = XLSX.read(data, {type: "binary"})
+            wb.SheetNames.forEach(sheet => {
+                let rowObj = XLSX.utils.sheet_to_row_object_array(wb.Sheets[sheet])
+                console.log(rowObj)
+                
+            })
+
+            // console.log(wb)
+        }
+    }
 }]
